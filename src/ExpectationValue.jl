ed5488b86e22c2b39bb30893e7a6a7227616496c
ttolppanen <taneli.tolppanen@gmail.com>
2022-11-10 11:29:57 +0200

    fixed expectation value imag check

--- src/ExpectationValue.jl
+++ src/ExpectationValue.jl
@@ -2,7 +2,7 @@ export expval
 
 function expval(state::AbstractVector{<:Number}, op::AbstractMatrix{<:Number})
     out = state' * op * state
-    return imag(out) == 0.0 ? real(out) : throw(InexactError(:Real, Real, out))
+    return isapprox(imag(out), 0; atol=eps(Float64)) ? real(out) : throw(InexactError(:Real, Real, out)) #Check if imaginary is zero
 end
 
 function expval(states::AbstractVector{<:AbstractVector{<:Number}}, op::AbstractMatrix{<:Number})
