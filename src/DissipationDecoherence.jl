2416e4aba847882b3d94b8de1e6c14c16fc31aac
ttolppanen <taneli.tolppanen@gmail.com>
2024-05-30 17:03:53 +0300

    fixed comment

--- src/DissipationDecoherence.jl
+++ src/DissipationDecoherence.jl
@@ -4,7 +4,7 @@
 export apply_diss_deco!
 
 # operators : list of operators related to dissipation and decoherence, which should be multiplied by
-#             the corresponding rates, i.e. kappa * a.
+#             the corresponding rates, i.e. sqrt(kappa) * a.
 
 function apply_diss_deco!(state::AbstractVector{<:Number}, operators::Vector{<:AbstractMatrix}; normalize = true)::Int64
     probabilities = []
