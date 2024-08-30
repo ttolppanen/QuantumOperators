db7c47fe308f1c9fbce13c5a14caf1ea84f5b123
ttolppanen <taneli.tolppanen@gmail.com>
2024-08-30 14:47:18 +0300

    added diss deco in subspace

--- src/DissipationDecoherence.jl
+++ src/DissipationDecoherence.jl
@@ -22,3 +22,41 @@ function apply_diss_deco!(state::AbstractVector{<:Number}, operators::Vector{<:A
     end
     return result
 end
+# diss_op and deco_op should be of the form Vector{<:Vector{<:AbstractMatrix}}
+function apply_diss_deco!(state::Vector{<:AbstractVector{<:Number}}, subspace_id::Integer, diss_op, deco_op; normalize = true)::Tuple{Int64, Int64}
+    probabilities = []
+    n_diss = length(diss_op)
+    n_deco = length(deco_op)
+
+    # calculating probabilities
+    for i in 1:n_diss
+        op = diss_op[i]
+        prob = norm(op[subspace_id] * state[subspace_id])^2 # norm^2 = <psi|psi>
+        push!(probabilities, prob)
+    end
+    for i in 1:n_deco
+        op = deco_op[i]
+        prob = norm(op[subspace_id] * state[subspace_id])^2 # norm^2 = <psi|psi>
+        push!(probabilities, prob)
+    end
+
+    # normalizing and drawing the result
+    probabilities .= probabilities ./ sum(probabilities)
+    result = findfirst(cumsum(real.(probabilities)) .> rand())
+
+    # if dissipation happened, we need to change the subspace
+    if result <= length(diss_op)
+        new_id = subspace_id - 1
+        mul!(state[new_id], diss_op[result][subspace_id], state[subspace_id])
+        if normalize
+            normalize!(state[new_id])
+        end
+        return new_id, result
+    else
+        state[subspace_id] .= deco_op[result - n_diss][subspace_id] * state[subspace_id]
+        if normalize 
+            normalize!(state[subspace_id])
+        end
+        return subspace_id, result
+    end
+end
--- test/dissipationdecoherencetest.jl
+++ test/dissipationdecoherencetest.jl
@@ -20,7 +20,31 @@ end
         result = apply_diss_deco!(deepcopy(state), operators)
         push!(outcomes, result)
     end
-    @test abs(sum(outcomes .- 1) / n - 0.5) < 3 * 0.005 # 3 times standard deviation
+    @test abs(sum(outcomes .- 1) / n - 0.5) < 3 * 0.5 / sqrt(n) # 3 times standard deviation
+end
+
+@testset "Dissipation in Subspace" begin
+    d = 2; L = 2;
+    state = onezero(d, L)
+    
+    indices = total_boson_number_subspace_indices(d, L)
+    ranges, perm_mat = total_boson_number_subspace_tools(d, L)
+    id = find_subspace(state, indices)
+
+    state = subspace_split(state, indices)
+    a_relations = [-1 for _ in eachindex(ranges)]
+    a_relations[1] = 0
+    diss_op = [subspace_split(singlesite_a(d, L, 1), ranges, perm_mat, a_relations)]
+    deco_op = [subspace_split(singlesite_n(d, L, 1), indices)]
+    new_id, result = apply_diss_deco!(state, id, diss_op, deco_op)
+    if result == 1
+        @test new_id == id - 1
+        @test state[new_id] == [1.0]
+    elseif result == 2
+        @test new_id == id
+    else
+        @test false
+    end
 end
 
 end # testset
